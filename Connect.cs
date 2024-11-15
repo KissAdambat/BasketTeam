f60a0b13f43a28c690b75fb1b86eea336e15dae8
KissAdam <kissa@kkszki.hu>
2024-11-15 13:04:46 +0100

    first commit

---
+++ Connect.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace BasketBall
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "Team";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+            Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketBall
+{
+    internal class Program
+    {
+        public static Connect conn = new Connect();
+        public static void GetAllData()
+        {
+            conn.Connection.Open();
+
+            string sql = "SELECT * FROM `player`";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read()) 
+            { 
+
+                var player = new 
+                { 
+                 Id = dr.GetInt32(0),
+                 Name = dr.GetString(1),
+                 Height = dr.GetInt32(2),
+                 Weight = dr.GetInt32(3),
+                 CreatedTime = dr.GetDateTime(4)
+                };
+
+                Console.WriteLine($"Játékos adatok: {player.Name},{player.Height}, {player.Weight}, {player.CreatedTime}");
+            }
+            dr.Close();
+            conn.Connection.Close();
+
+        } 
+        static void Main(string[] args)
+        {
+            GetAllData();
+        }
+    }
+}
